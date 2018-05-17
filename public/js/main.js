6d1509cda2fb314f7b7fc394f1a3e67b4144e452
MeliaNolen <mnolen5670@gmail.com>
2018-05-17 14:16:14 -0400

    set up navbar and full screen hero image

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    console.log('Main JS active');
+    $(document).foundation();
+    $('.top-bar').on('sticky.zf.stuckto:top', function(){
+        $(this).addClass('shrink');
+      }).on('sticky.zf.unstuckfrom:top', function(){
+        $(this).removeClass('shrink');
+    });
+
+    function fullscreen(){
+		jQuery('#hero').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+	}
+  
+	fullscreen();
+
+    // Run the function in case of window resize
+    jQuery(window).resize(function() {
+        fullscreen();         
+    });
 });
